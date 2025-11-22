52333d999e34ebd5306060c6d67d9ea7c04c8684
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-11-22 20:53:10 +0100

    feat(acc-service): added more config

--- AccommodationService/AccommodationService/Program.cs
+++ AccommodationService/AccommodationService/Program.cs
@@ -1,14 +1,15 @@
 using System.Text;
 using AccommodationService.Configuration;
+using AccommodationService.Data;
+using AccommodationService.Infrastructure;
 using AccommodationService.Services;
 using AccommodationService.Services.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
@@ -29,21 +30,44 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ClockSkew = TimeSpan.Zero
         };
     });
-
+builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+// TODO check
+// builder.Services.AddSwaggerGenWithAuth();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// TODO check
+// builder.Services.AddUserServiceDependencies();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+
 builder.Services.AddAuthorization();
-
 var app = builder.Build();
+if (!app.Environment.IsEnvironment("Test"))
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        db.Database.Migrate();
+    }
+}
 
-// Configure the HTTP request pipeline.
+app.UseRouting();
+app.UseExceptionHandler();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
-
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.MapGet("/health", () => "OK");
 app.Run();

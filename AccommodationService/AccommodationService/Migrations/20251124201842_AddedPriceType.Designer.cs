3db7cffb0e9801875bc427509dbf2ff238e7fe11
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-11-24 21:19:13 +0100

    feat(acc-avail): Added new migration

--- AccommodationService/AccommodationService/Migrations/20251124201842_AddedPriceType.Designer.cs
+++ AccommodationService/AccommodationService/Migrations/20251124201842_AddedPriceType.Designer.cs
@@ -4,6 +4,7 @@ using AccommodationService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AccommodationService.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251124201842_AddedPriceType")]
+    partial class AddedPriceType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

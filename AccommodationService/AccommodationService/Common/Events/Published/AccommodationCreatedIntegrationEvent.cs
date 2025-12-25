94e68371cd172f29c20bf562c76d1e9df1748f62
Bogdan Janosevic <96696175+BogdanJ33@users.noreply.github.com>
2025-12-25 19:52:28 +0100

    fix(price): Implemented calculate price and fixed accommodation created event (#10)

--- AccommodationService/AccommodationService/Common/Events/Published/AccommodationCreatedIntegrationEvent.cs
+++ AccommodationService/AccommodationService/Common/Events/Published/AccommodationCreatedIntegrationEvent.cs
@@ -1,6 +1,7 @@
 ﻿using AccommodationService.Domain.DTOs;
+using AccommodationService.Domain.Enums;
 
 namespace AccommodationService.Common.Events.Published
 {
-    public record AccommodationCreatedIntegrationEvent(Guid AccommodationId, string Name, int MaxGuest, int MinGuest, List<AmenityDTO> Amenities, LocationDTO? Location) : IIntegrationEvent;
+    public record AccommodationCreatedIntegrationEvent(Guid AccommodationId, string Name, int MaxGuest, int MinGuest, List<AmenityDTO> Amenities, LocationDTO? Location, PriceType PriceType) : IIntegrationEvent;
 }
